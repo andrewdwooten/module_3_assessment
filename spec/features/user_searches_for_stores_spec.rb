264a81932b281b68f3c512f4518aaac1f4d75f8e
Andy Wooten <andrewdwooten@icloud.com>
2017-04-13 11:39:54 -0600

    Add additional feature test assertions
    Adds additional feature test assertions to confirm that
    pagination of store search results is working correctly.

--- spec/features/user_searches_for_stores_spec.rb
+++ spec/features/user_searches_for_stores_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 feature 'user visits root and searches for best buys' do
-  scenario 'user is directed to /search and can see stores' do
+  scenario 'user is directed to /search and can see stores which are paginated' do
     visit '/'
 
     expect(page).to have_selector('#search-form')
@@ -20,6 +20,22 @@ feature 'user visits root and searches for best buys' do
       expect(page).to have_content('DENVER')
       expect(page).to have_content('Mobile SAS')
       expect(page).to have_content('3.45')
+    end
+
+    expect(page).to have_link('2')
+
+    click_link '2'
+
+    expect(current_path).to eq('/search')
+    expect(page).to have_content("16 Total Stores")
+    expect(page).to have_selector("#stores_container")
+    expect(page).to have_selector("#store_instance", count: 6)
+
+    within all('#store_instance').first do
+      expect(page).to have_content('BEST BUY - I70 & TOWER RD')
+      expect(page).to have_content('AURORA')
+      expect(page).to have_content('Big Box')
+      expect(page).to have_content('11.65')
     end
   end
 end
