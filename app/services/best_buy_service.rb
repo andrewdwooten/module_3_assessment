8d711e47b1262cf6169dbb9ae1733558e2903cea
Andy Wooten <andrewdwooten@icloud.com>
2017-04-13 10:48:32 -0600

    Build out service
    Builds out necessary method from service to hit API endpoint as
    desired. Test is currently passing with vcr implemented.

--- app/services/best_buy_service.rb
+++ app/services/best_buy_service.rb
@@ -1,5 +1,21 @@
 class BestBuyService
+  attr_reader :conn, :auth, :show, :type, :page_size
 
   def initialize
-    @conn = Faraday.new('https://api.bestbuy.com/v1/')
+    @conn = Faraday.new('https://api.bestbuy.com')
+    @auth = "apiKey=#{ENV['STORE_KEY']}"
+    @show = 'show=longName,storeType,city,distance'
+    @type = 'format=json'
+    @page_size = 'pageSize=16'
+  end
+
+  def get_stores_by_zip(zip)
+    parse(conn.get("/v1/stores(area(#{zip},25))?#{type}&#{show}&#{page_size}&#{auth}"))
+  end
+
+  private
+
+  def parse(response)
+    JSON.parse(response.body, symbolize_names: true)
+  end
 end
--- spec/services/best_buy_service_spec.rb
+++ spec/services/best_buy_service_spec.rb
@@ -2,6 +2,16 @@ require 'rails_helper'
 
 describe BestBuyService do
   it 'returns a list of stores within 25 miles of zip code' do
-    searcher = BestBuyService.new
+    VCR.use_cassette('services/bestbuy') do
+      searcher = BestBuyService.new
+      raw_stores = searcher.get_stores_by_zip('80202')[:stores]
+      first_store = raw_stores.first
+
+      expect(raw_stores.count).to eq(16)
+      expect(first_store.has_key?(:longName)).to eq(true)
+      expect(first_store.has_key?(:storeType)).to eq(true)
+      expect(first_store.has_key?(:distance)).to eq(true)
+      expect(first_store.has_key?(:city)).to eq(true)
+    end
   end
 end
