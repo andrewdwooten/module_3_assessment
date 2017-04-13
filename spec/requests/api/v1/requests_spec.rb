a04b97df87e215aaaf4393ff8fda714e6c296910
Andy Wooten <andrewdwooten@icloud.com>
2017-04-13 09:21:05 -0600

    Complete user story 1
    Completes testing of user story one. Get
    request sent to api/v1/items will return
    all items with only the id, name, description,
    and image_url of each item. Test is currently
    passing.

--- spec/requests/api/v1/requests_spec.rb
+++ spec/requests/api/v1/requests_spec.rb
@@ -13,7 +13,23 @@ describe 'api v1 requests' do
   it 'can return all items' do
 
     get 'api/v1/items'
+    items = JSON.parse(response.body)
+    first = items.first
+    last = items.last
 
-    byebug
+    expect(response.status).to eq(200)
+
+    expect(first['name']).to eq('test1')
+    expect(first['description']).to eq('cool_thing1')
+    expect(first['image_url']).to eq('image1')
+    expect(first.has_key?('created_at')).to eq(false)
+    expect(first.has_key?('udpated_at')).to eq(false)
+
+
+    expect(last['name']).to eq('test2')
+    expect(last['description']).to eq('cool_thing2')
+    expect(last['image_url']).to eq('image2')
+    expect(last.has_key?('created_at')).to eq(false)
+    expect(last.has_key?('udpated_at')).to eq(false)
   end
 end
