7b047422fa9ec8324131e356029bc7416e1b6e54
Andy Wooten <andrewdwooten@icloud.com>
2017-04-13 11:51:45 -0600

    Modify testing assertions in requests_spec
    Modify testing assertions to be less literal and to
    reference the two items created in the before action.

--- spec/requests/api/v1/requests_spec.rb
+++ spec/requests/api/v1/requests_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe 'api v1 requests' do
+describe 'api v1' do
   attr_reader :item, :item2
   before(:each) do
     @item = Item.create(name:        'test1',
@@ -10,75 +10,77 @@ describe 'api v1 requests' do
                         description: 'cool_thing2',
                         image_url:   'image2')
   end
-  it 'can return all items' do
-    VCR.use_cassette('requests/api/v1/items_index') do
-      get 'api/v1/items'
-      items = JSON.parse(response.body)
-      first = items.first
-      last = items.last
+describe 'requests' do
+    it 'can return all items' do
+      VCR.use_cassette('requests/api/v1/items_index') do
+        get 'api/v1/items'
+        items = JSON.parse(response.body)
+        first = items.first
+        last = items.last
 
-      expect(response.status).to eq(200)
+        expect(response.status).to eq(200)
 
-      expect(first['name']).to eq('test1')
-      expect(first['description']).to eq('cool_thing1')
-      expect(first['image_url']).to eq('image1')
-      expect(first.has_key?('created_at')).to eq(false)
-      expect(first.has_key?('udpated_at')).to eq(false)
+        expect(first['name']).to eq(item.name)
+        expect(first['description']).to eq(item.description)
+        expect(first['image_url']).to eq(item.image_url)
+        expect(first.has_key?('created_at')).to eq(false)
+        expect(first.has_key?('udpated_at')).to eq(false)
 
 
-      expect(last['name']).to eq('test2')
-      expect(last['description']).to eq('cool_thing2')
-      expect(last['image_url']).to eq('image2')
-      expect(last.has_key?('created_at')).to eq(false)
-      expect(last.has_key?('udpated_at')).to eq(false)
+        expect(last['name']).to eq(item2.name)
+        expect(last['description']).to eq(item2.description)
+        expect(last['image_url']).to eq(item2.image_url)
+        expect(last.has_key?('created_at')).to eq(false)
+        expect(last.has_key?('udpated_at')).to eq(false)
+      end
     end
-  end
 
-  it 'can return an item' do
-    VCR.use_cassette('requests/api/v1/item_show') do
-      get 'api/v1/items/1'
-      item = JSON.parse(response.body)
+    it 'can return an item' do
+      VCR.use_cassette('requests/api/v1/item_show') do
+        get 'api/v1/items/1'
+        response_item = JSON.parse(response.body)
 
-      expect(response.status).to eq(200)
+        expect(response.status).to eq(200)
 
-      expect(item['name']).to eq('test1')
-      expect(item['description']).to eq('cool_thing1')
-      expect(item['image_url']).to eq('image1')
-      expect(item.has_key?('created_at')).to eq(false)
-      expect(item.has_key?('udpated_at')).to eq(false)
+        expect(response_item['name']).to eq(item.name)
+        expect(response_item['description']).to eq(item.description)
+        expect(response_item['image_url']).to eq(item.image_url)
+        expect(response_item.has_key?('created_at')).to eq(false)
+        expect(response_item.has_key?('udpated_at')).to eq(false)
 
+      end
     end
-  end
 
-  it 'can delete an item' do
-    VCR.use_cassette('requests/api/v1/item_delete') do
+    it 'can delete an item' do
+      VCR.use_cassette('requests/api/v1/item_delete') do
 
-      expect(Item.count).to eq(2)
-      delete 'api/v1/items/1'
+        expect(Item.count).to eq(2)
+        delete 'api/v1/items/1'
 
-      expect(response.status).to eq(204)
-      expect(Item.count).to eq(1)
-      expect(Item.where(id: item.id).empty?).to eq(true)
+        expect(response.status).to eq(204)
+        expect(Item.count).to eq(1)
+        expect(Item.where(id: item.id).empty?).to eq(true)
+      end
     end
-  end
 
-  it 'can create an item' do
-    VCR.use_cassette('requests/api/v1/item_create') do
-      creation_params =  {item: {name:        'create_test_name',
-                                 description: 'create_test_desc',
-                                 image_url:   'create_test_img'}
-                         }
-      post 'api/v1/items', creation_params
+    it 'can create an item' do
+      VCR.use_cassette('requests/api/v1/item_create') do
+        creation_params =  {item: {name:        'create_test_name',
+                                   description: 'create_test_desc',
+                                   image_url:   'create_test_img'}
+                           }
+        post 'api/v1/items', creation_params
 
 
-        expect(Item.count).to eq(3)
+          expect(Item.count).to eq(3)
 
-        test_item = Item.last
+          test_item = Item.last
 
-        expect(response.status).to eq(201)
-        expect(test_item['name']).to eq('create_test_name')
-        expect(test_item['description']).to eq('create_test_desc')
-        expect(test_item['image_url']).to eq('create_test_img')
+          expect(response.status).to eq(201)
+          expect(test_item['name']).to eq('create_test_name')
+          expect(test_item['description']).to eq('create_test_desc')
+          expect(test_item['image_url']).to eq('create_test_img')
+      end
     end
   end
 end
